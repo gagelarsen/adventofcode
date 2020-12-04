7895805a4f016d259581c73b202de36236efd88a
Gage Larsen <gagelarsen53@gmail.com>
2020-12-03 21:42:16 -0700

    Add code stubs and files for Day 04 of the advent.

---
+++ solutions/day04.py
@@ -0,0 +1,22 @@
+"""
+Day 04 of the Advent of Code Challenge
+
+Author: Gage Larsen
+Date: December 3, 2020
+"""
+
+
+def puzzle_function():
+    return None
+
+
+def main():
+    data = puzzle_data
+    print(f'The answer to part 1 is: {data}')
+
+
+example_data = []
+puzzle_data = []
+
+if __name__ == '__main__':
+    main()
