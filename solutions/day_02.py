f22a396988a2398bdbd14916355908d6d6704a9c
gagelarsen <gagelarsen53@gmail.com>
2021-12-01 22:25:10 -0700

    day #2

--- solutions/day_02.py
+++ solutions/day_02.py
@@ -6,11 +6,34 @@ from day_base import AdventDay
 class Day02(AdventDay):
 
     def answer_01(self, input_data: object) -> object:
-        answer = '<no answer>'
-        return answer
+        x = y = 0
+
+        direction_map = {
+            'up': lambda _x, _y, _count: (_x,  _y - _count),
+            'down': lambda _x, _y, _count: (_x,  _y + _count),
+            'forward': lambda _x, _y, _count: (_x + _count, _y),
+            'backward': lambda _x, _y, _count: (_x - _count, _y),
+        }
+
+        for item in input_data:
+            direction, count = item.split(' ')
+            x, y = direction_map[direction](x, y, int(count))
+        return x * y
 
     def answer_02(self, input_data: object) -> object:
-        answer = '<no answer>'
+        x = y = aim = 0
+
+        direction_map = {
+            'up': lambda _x, _y, _aim, _count: (_x,  _y, _aim - _count),
+            'down': lambda _x, _y, _aim, _count: (_x,  _y, _aim + _count),
+            'forward': lambda _x, _y, _aim, _count: (_x + _count,  _y + (_aim * _count), _aim),
+            'backward': lambda _x, _y, _aim, _count: (_x + _count,  _y - (_aim * _count), _aim),
+        }
+
+        for item in input_data:
+            direction, count = item.split(' ')
+            x, y, aim = direction_map[direction](x, y, aim, int(count))
+        answer = x * y
         return answer
 
 
